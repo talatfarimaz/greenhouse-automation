88b1c0ef3ead43d531154949dd19105467349f22
talat farımaz <11904012+talatfarimaz@users.noreply.github.com>
2017-11-27 23:45:08 +0300

    Update panel.php

--- panel.php
+++ panel.php
@@ -26,6 +26,8 @@ sleep(2);
                 background-image: url(14.jpg);
                 background-position: center top;
                 background-size: 100% auto;
+                font-family: 'Lato', sans-serif;
+
             }
 
 
@@ -53,17 +55,34 @@ sleep(2);
             .box {
                 border: #666666 solid 1px;
             }
-            
+
             .rcorners2 {
                 border-radius: 25px;
                 border: 2px solid black;
 
             }
-            
+
             .text {
                 text-align: center;
             }
 
+            .rcorners1 {
+                border-radius: 15px 15px;
+                border: 3px solid #66ccff;
+                font-size: 18px;
+
+            }
+            
+            .rcorners3 {
+                border-radius: 15px 15px;
+                border: 3px solid #b3ffda;
+                font-size: 18px;
+
+            }
+            
+            
+
+            
 
         </style>
 
@@ -87,52 +106,61 @@ sleep(2);
             <div class="rcorners2" style="width:350px; border: solid 1px #333333; background-color:#e6ffe6" align="left">
                 <div class="rcorners2" style="background-color:black; clear:both; text-align:center; color:darkcyan; padding:10px;"><b>Kontrol Paneli</b></div>
 
-                <div  style="margin:50px">
+                <div style="margin:50px">
                     <form action="panel.php" method="post">
-                        <strong>-----------------------</strong>
-                        <div style="background-color:#e6ffe6; clear:both; text-align:left; color:darkcyan; padding:10px;"><b>VANA</b></div><br>
-                        <div>
-                            <input type="radio" name="aç-kapat1" value="aç" />&nbsp;&nbsp;
-                            <font color="green"><strong>AÇ</strong></font>
+
+                        <div style="background-color:#e6ffe6; clear:both; text-align:center; color:darkcyan; padding:10px;"><b>VANA</b></div><br>
+                        <div class="rcorners1" align="center">
+                            <input type="radio" name="aç-kapat1" value="aç" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="radio" name="aç-kapat1" value="kapat" />
+
                         </div><br>
 
-                        <div>
-                            <input type="radio" name="aç-kapat1" value="kapat" />&nbsp;&nbsp;
-                            <font color="red"><strong>KAPAT</strong></font>
-                        </div> <strong>-----------------------</strong>
-                        <div style="background-color:#e6ffe6; clear:both; text-align:left; color:darkcyan; padding:10px;"><b>FAN</b></div><br>
-                        <div>
-                            <input type="radio" name="aç-kapat2" value="aç" />&nbsp;&nbsp;
-                            <font color="green"><strong>AÇ</strong></font>
+                        <div class="rcorners1" align="center">
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <font color="#00802b"><strong>AÇ</strong></font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                            <font color="#b30000"><strong>KAPAT</strong></font>
+                        </div><br><br>
+                        <div style="background-color:#e6ffe6; clear:both; text-align:center; color:darkcyan; padding:10px;"><b>FAN</b></div><br>
+                        <div class="rcorners1" align="center">
+                            <input type="radio" name="aç-kapat2" value="aç" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="radio" name="aç-kapat2" value="kapat" />
+
                         </div><br>
 
-                        <div>
-                            <input type="radio" name="aç-kapat2" value="kapat" />&nbsp;&nbsp;
-                            <font color="red"><strong>KAPAT</strong></font>
-                        </div> <strong>-----------------------</strong>
-                        <div style="background-color:#e6ffe6; clear:both; text-align:left; color:darkcyan; padding:10px;"><b>HAVALANDIRMA</b></div><br>
-                        <div>
-                            <input type="radio" name="aç-kapat3" value="aç" />&nbsp;&nbsp;
-                            <font color="green"><strong>AÇ</strong></font>
+                        <div class="rcorners1" align="center">
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <font color="#00802b"><strong>AÇ</strong></font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                            <font color="#b30000"><strong>KAPAT</strong></font>
+                        </div><br><br>
+                        <div style="background-color:#e6ffe6; clear:both; text-align:center; color:darkcyan; padding:10px;"><b>HAVALANDIRMA</b></div><br>
+                        <div class="rcorners1" align="center">
+                            <input type="radio" name="aç-kapat3" value="aç" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="radio" name="aç-kapat3" value="kapat" />
+
                         </div><br>
 
-                        <div>
-                            <input type="radio" name="aç-kapat3" value="kapat" />&nbsp;&nbsp;
-                            <font color="red"><strong>KAPAT</strong></font>
-                        </div><strong>-----------------------</strong>
+                        <div class="rcorners1" align="center">
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <font color="#00802b"><strong>AÇ</strong></font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-                        <div>
+                            <font color="#b30000"><strong>KAPAT</strong></font>
+                        </div>
+
+                        <div align="center">
                             <br>
-                            <input type="submit" style="text-align:center; color:darkcyan; padding:3px;" value="Uygula">&nbsp;&nbsp;
-                            <input type="button" style="text-align:center; color:darkcyan; padding:3px;" value="Geri" onclick="location='welcome.php'" />
-                        </div><br>
-                        <div>
+                            <input align="center" class="rcorners3" type="submit" style="text-align:center; color:darkcyan; padding:3px;" value="Uygula">&nbsp;&nbsp;
+                            <input align="center" class="rcorners3" type="button" style="text-align:center; color:darkcyan; padding:3px;" value="Geri" onclick="location='welcome.php'" />
+                        </div><br><br>
+                        <div align="center">
                             <?php
 
 
                             if ($_POST["aç-kapat1"]=="aç")
                             {
-                                echo '<font color="green" ><strong>Vana Açık</strong></font><br>';
+                                echo '<font color="#00802b" ><strong>Vana Açık</strong></font><br><br>';
                                 fwrite($port1, "A");
                                 $vanaac = "UPDATE `sys`.`vana_havalandırma` SET `vana`='açık' WHERE `kisi_id_vh`='$id'";
                                 $change = $conn->query($vanaac);
@@ -140,7 +168,7 @@ sleep(2);
 
                             if ($_POST["aç-kapat1"]=="kapat")
                             {
-                                echo '<font color="red"><strong>Vana Kapalı</strong></font><br>';
+                                echo '<font color="#b30000"><strong>Vana Kapalı</strong></font><br><br>';
                                 fwrite($port1, "B");
                                 $vanakapat = "UPDATE `sys`.`vana_havalandırma` SET `vana`='kapalı' WHERE `kisi_id_vh`='$id'";
                                 $change = $conn->query($vanakapat);
@@ -148,7 +176,7 @@ sleep(2);
 
                             if ($_POST["aç-kapat2"]=="aç")
                             {
-                                echo '<font color="green"><strong>Fan Açık</strong></font><br>';
+                                echo '<font color="#00802b"><strong>Fan Açık</strong></font><br><br>';
                                 fwrite($port1, "C");
                                 $fanac = "UPDATE `sys`.`vana_havalandırma` SET `fan`='açık' WHERE `kisi_id_vh`='$id'";
                                 $change = $conn->query($fanac);
@@ -156,14 +184,14 @@ sleep(2);
 
                             if ($_POST["aç-kapat2"]=="kapat")
                             {
-                                echo '<font color="red"><strong>Fan Kapalı</strong></font><br>';
+                                echo '<font color="#b30000"><strong>Fan Kapalı</strong></font><br><br>';
                                 fwrite($port1, "D");
                                 $fankapat = "UPDATE `sys`.`vana_havalandırma` SET `fan`='kapalı' WHERE `kisi_id_vh`='$id'";
                                 $change = $conn->query($fankapat);
                             }
                             if ($_POST["aç-kapat3"]=="aç")
                             {
-                                echo '<font color="green"><strong>Havalandırma Açık</strong></font><br>';
+                                echo '<font color="#00802b"><strong>Havalandırma Açık</strong></font><br><br>';
                                 fwrite($port1, "E");
                                 $havaac = "UPDATE `sys`.`vana_havalandırma` SET `havalandirma`='açık' WHERE `kisi_id_vh`='$id'";
                                 $change = $conn->query($havaac);
@@ -171,7 +199,7 @@ sleep(2);
 
                             if ($_POST["aç-kapat3"]=="kapat")
                             {
-                                echo '<font color="red"><strong>Havalandırma Kapalı</strong></font><br>';             
+                                echo '<font color="#b30000"><strong>Havalandırma Kapalı</strong></font><br><br>';             
                                 fwrite($port1, "F");
                                 $havakapat = "UPDATE `sys`.`vana_havalandırma` SET `havalandirma`='kapalı' WHERE `kisi_id_vh`='$id'";
                                 $change = $conn->query($havakapat);
